765461ed34e104acea62a55028f8b53dab4db6b4
tczyz <tomczyz97@gmail.com>
2022-08-17 00:31:52 +0200

    add AhoCorasick algorithm and partial matching endpoint

---
+++ TagTool.Backend.Tests.Unit/StringExtensionsTests.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using TagTool.Backend.Services;
+using Xunit;
+
+namespace TagTool.Backend.Tests.Unit;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void GetAllSubstrings_ValidWord_CorrectReturn()
+    {
+        // Arrange
+        var expectedSubstrings = new[] { "w", "o", "r", "d", "wo", "or", "rd", "wor", "ord", "word" };
+
+        // Act
+        var allSubstrings = "word".GetAllSubstrings();
+
+        // Assert
+        allSubstrings.Should().HaveCount(10);
+        allSubstrings.Should().Equal(expectedSubstrings);
+    }
+}
--- src/TagTool.Backend/Services/TagSearchService.cs
+++ src/TagTool.Backend/Services/TagSearchService.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using Ganss.Text;
+using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
 using TagTool.Backend.DbContext;
 
@@ -22,6 +23,33 @@ public class TagSearchService : Backend.TagSearchService.TagSearchServiceBase
         }
     }
 
+    public override async Task MatchTags(
+        MatchTagsRequest request,
+        IServerStreamWriter<MatchTagsReply> responseStream,
+        ServerCallContext context)
+    {
+        var dict = request.PartialTagName.GetAllSubstrings();
+        var ahoCorasick = new AhoCorasick(dict);
+
+        var tagNames = await GetAllTags(ct: context.CancellationToken);
+
+        foreach (var tagName in tagNames)
+        {
+            var wordMatches = ahoCorasick.Search(tagName).ToList();
+
+            // todo: filter search results to exclude overlying substrings 
+            if (wordMatches.Count == 0) continue;
+
+            var matchTagsReply = new MatchTagsReply { TagName = tagName };
+            foreach (var match in wordMatches)
+            {
+                matchTagsReply.MatchedParts.Add(new MatchedPart { StartIndex = match.Index, Length = match.Word.Length });
+            }
+
+            await responseStream.WriteAsync(matchTagsReply, context.CancellationToken);
+        }
+    }
+
     private static async Task<IEnumerable<string>> GetTags(string partialTagName, int requestMaxReturn, CancellationToken ct)
     {
         await using var db = new TagContext();
@@ -35,5 +63,38 @@ public class TagSearchService : Backend.TagSearchService.TagSearchServiceBase
             .ToListAsync(cancellationToken: ct);
 
         return query;
+    }
+
+    // todo: adjust this method for batch processing for case when we have extremely large number of tags (what is extremely large?)
+    private static async Task<IEnumerable<string>> GetAllTags(CancellationToken ct = default)
+    {
+        await using var db = new TagContext();
+
+        var query = await db.Tags
+            .AsNoTracking()
+            .Select(tag => tag.Name)
+            .ToListAsync(ct);
+
+        return query;
+    }
+}
+
+public static class StringExtensions // todo: optimize with span<char>
+{
+    public static string[] GetAllSubstrings(this string word)
+    {
+        var substrings = new string[((1 + word.Length) * word.Length) / 2];
+
+        var counter = 0;
+        for (var substringLength = 1; substringLength <= word.Length; ++substringLength)
+        {
+            for (var startIndex = 0; startIndex <= word.Length - substringLength; startIndex++)
+            {
+                substrings[counter] = word.Substring(startIndex, substringLength);
+                counter++;
+            }
+        }
+
+        return substrings;
     }
 }
