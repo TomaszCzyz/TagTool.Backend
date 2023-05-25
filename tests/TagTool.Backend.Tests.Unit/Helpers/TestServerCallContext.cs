cfb48578d3b3ceb06ff630120e4016d2b28957db
tczyz <tomczyz97@gmail.com>
2023-05-26 01:04:44 +0200

    add tests for TagService

---
+++ tests/TagTool.Backend.Tests.Unit/Helpers/TestServerCallContext.cs
@@ -0,0 +1,50 @@
+﻿using Grpc.Core;
+
+namespace TagTool.Backend.Tests.Unit.Helpers;
+
+public sealed class TestServerCallContext : ServerCallContext
+{
+    private readonly Dictionary<object, object> _userState;
+
+    public Metadata? ResponseHeaders { get; private set; }
+
+    private TestServerCallContext(Metadata requestHeaders, CancellationToken cancellationToken)
+    {
+        RequestHeadersCore = requestHeaders;
+        CancellationTokenCore = cancellationToken;
+        DeadlineCore = DateTime.Now.AddHours(1);
+        ResponseTrailersCore = new Metadata();
+        AuthContextCore = new AuthContext(string.Empty, new Dictionary<string, List<AuthProperty>>());
+        _userState = new Dictionary<object, object>();
+    }
+
+    protected override string MethodCore => "MethodName";
+    protected override string HostCore => "HostName";
+    protected override string PeerCore => "PeerName";
+    protected override DateTime DeadlineCore { get; }
+    protected override Metadata RequestHeadersCore { get; }
+    protected override CancellationToken CancellationTokenCore { get; }
+    protected override Metadata ResponseTrailersCore { get; }
+    protected override Status StatusCore { get; set; }
+    protected override WriteOptions? WriteOptionsCore { get; set; }
+    protected override AuthContext AuthContextCore { get; }
+
+    protected override IDictionary<object, object> UserStateCore => _userState;
+
+    protected override ContextPropagationToken CreatePropagationTokenCore(ContextPropagationOptions? options)
+        => throw new NotImplementedException();
+
+    protected override Task WriteResponseHeadersAsyncCore(Metadata responseHeaders)
+    {
+        if (ResponseHeaders != null)
+        {
+            throw new InvalidOperationException("Response headers have already been written.");
+        }
+
+        ResponseHeaders = responseHeaders;
+        return Task.CompletedTask;
+    }
+
+    public static TestServerCallContext Create(Metadata? requestHeaders = null, CancellationToken cancellationToken = default)
+        => new(requestHeaders ?? new Metadata(), cancellationToken);
+}
---
+++ tests/TagTool.Backend.Tests.Unit/Services/Grpc/TagServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Google.Protobuf.WellKnownTypes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TagTool.Backend.DbContext;
+using TagTool.Backend.DomainTypes;
+using TagTool.Backend.Models;
+using TagTool.Backend.Services;
+using TagTool.Backend.Tests.Unit.Helpers;
+using Xunit;
+using TaggedItemDto = TagTool.Backend.Models.TaggedItem;
+using YearTag = TagTool.Backend.Models.YearTag;
+using YearTagDto = TagTool.Backend.DomainTypes.YearTag;
+
+namespace TagTool.Backend.Tests.Unit.Services.Grpc;
+
+public class TagServiceTests
+{
+    private readonly Backend.Services.Grpc.TagService _tagService;
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly Mock<ICommandsHistory> _commandsHistoryMock;
+    private readonly TagToolDbContext _dbContextMock;
+
+    public TagServiceTests()
+    {
+        var loggerMock = new Mock<ILogger<Backend.Services.Grpc.TagService>>();
+
+        _mediatorMock = new Mock<IMediator>();
+        _commandsHistoryMock = new Mock<ICommandsHistory>();
+
+        var optionsBuilder = new DbContextOptionsBuilder<TagToolDbContext>().UseInMemoryDatabase(databaseName: "TagToolDb").Options;
+
+        _dbContextMock = new TagToolDbContext(optionsBuilder);
+
+        _tagService = new Backend.Services.Grpc.TagService(
+            loggerMock.Object,
+            _mediatorMock.Object,
+            _commandsHistoryMock.Object,
+            _dbContextMock);
+    }
+
+    private const string TestItemType = "TestItemType";
+    private const string TestItemIdentifier = "TestItemIdentifier";
+
+    [Fact]
+    public async Task TagItem_ValidRequest_ReturnsTaggedItem()
+    {
+        // Arrange
+        var testServerCallContext = TestServerCallContext.Create();
+        var tagItemRequest
+            = new TagItemRequest
+            {
+                Item = new Item { ItemType = TestItemType, Identifier = TestItemIdentifier }, Tag = Any.Pack(new YearTagDto { Year = 4022 })
+            };
+
+        var taggedItem = new TaggedItemDto
+        {
+            ItemType = TestItemType,
+            UniqueIdentifier = TestItemIdentifier,
+            Tags = new List<TagBase>()
+        };
+
+        _mediatorMock
+            .Setup(m => m.Send(It.IsAny<Commands.TagItemRequest>(), testServerCallContext.CancellationToken))
+            .ReturnsAsync(taggedItem);
+
+        // Act
+        var response = await _tagService.TagItem(tagItemRequest, testServerCallContext);
+
+        // Assert
+        _mediatorMock.Verify(x => x.Send(It.IsAny<Commands.TagItemRequest>(), testServerCallContext.CancellationToken), Times.Once);
+        _mediatorMock.Verify(m
+            => m.Send(
+                It.Is<Commands.TagItemRequest>(r => r.Tag is YearTag && r.ItemType == TestItemType && r.Identifier == TestItemIdentifier),
+                It.IsAny<CancellationToken>()));
+
+        response.TaggedItem.Should().Be(taggedItem);
+    }
+}
