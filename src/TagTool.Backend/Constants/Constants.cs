92ee180946ce191c81cf9bc7e479404db47124bf
tczyz <tomczyz97@gmail.com>
2023-09-13 14:35:58 +0200

    Update ApplicationName and BasePath constants
    Renamed the 'ApplicationName' constant from 'TagToolBackend' to 'TagTool' and 'BasePath' subdirectory from 'Sqlite' to 'Backend'. This change aligns better with the naming conventions of the project and supports an improved structure for data organization. Removed the 'LogsDbPath' which is no longer in use.

--- src/TagTool.Backend/Constants/Constants.cs
+++ src/TagTool.Backend/Constants/Constants.cs
@@ -2,16 +2,14 @@
 
 public static class Constants
 {
-    private const string ApplicationName = "TagToolBackend";
+    private const string ApplicationName = "TagTool";
 
     private static readonly string _localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 
-    public static readonly string BasePath = Path.Join(_localAppDataPath, ApplicationName, "Sqlite");
+    public static readonly string BasePath = Path.Join(_localAppDataPath, ApplicationName, "Backend");
 
     // todo: move socket file location to AppData/.. 
     public static readonly string SocketPath = Path.Combine(Path.GetTempPath(), "socket.tmp");
 
     public static readonly string DbPath = Path.Join(BasePath, "TagTool.db");
-
-    public static readonly string LogsDbPath = Path.Combine(BasePath, "Logs", "log.db");
 }
