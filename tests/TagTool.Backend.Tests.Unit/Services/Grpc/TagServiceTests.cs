cfa49624e8f8eeca6951c95f0770f46056975d94
tczyz <tomczyz97@gmail.com>
2023-10-13 10:53:22 +0200

    Add unit tests for TagItem endpoint in TagService

--- tests/TagTool.Backend.Tests.Unit/Services/Grpc/TagServiceTests.cs
+++ tests/TagTool.Backend.Tests.Unit/Services/Grpc/TagServiceTests.cs
@@ -1,9 +1,11 @@
-﻿using Google.Protobuf.WellKnownTypes;
+﻿using FluentAssertions;
+using Google.Protobuf.WellKnownTypes;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using TagTool.Backend.DomainTypes;
 using TagTool.Backend.Mappers;
+using TagTool.Backend.Models;
 using TagTool.Backend.Services;
 using Xunit;
 using YearTagDto = TagTool.Backend.DomainTypes.YearTag;
@@ -12,11 +14,14 @@ namespace TagTool.Backend.Tests.Unit.Services.Grpc;
 
 public class TagServiceTests
 {
+    private const string ArbitraryErrorMessage = "Arbitrary error message.";
+
     private readonly Backend.Services.Grpc.TagService _sut;
 
     private readonly IMediator _mediator = Substitute.For<IMediator>();
     private readonly ITagMapper _tagMapper = TagMapperHelper.InitializeWithKnownMappers();
     private readonly ITaggableItemMapper _taggableItemMapper = new TaggableItemMapper();
+    private readonly TestServerCallContext _testServerCallContext = TestServerCallContext.Create();
 
     private readonly FileDto _fileDto = new() { Path = "TestItemIdentifier" };
     private readonly NormalTag _textTag = new() { Name = "TestTag1" };
@@ -35,19 +40,47 @@ public class TagServiceTests
     }
 
     [Fact]
-    public async Task TagItem_ValidRequest_ReturnsTaggedItem()
+    public async Task TagItem_ValidRequest_TaggedSuccessfully_ReturnsTaggedItem()
     {
         // Arrange
-        var testServerCallContext = TestServerCallContext.Create();
-        var tagItemRequest = new TagItemRequest { Item = new TaggableItemDto { File = _fileDto }, Tag = Any.Pack(_textTag) };
+        var taggableItemDto = new TaggableItemDto { File = _fileDto };
+        var existingItem = _taggableItemMapper.MapFromDto(taggableItemDto);
 
-        // TaggableFile??? and it turns out, that it is hard to test it due to weird TaggableItem in-method mapping...
+        var tagItemRequest = new TagItemRequest { Item = taggableItemDto, Tag = Any.Pack(_textTag) };
 
-        // _mediator.Send(Arg.Any<IRequest>()).Returns(info => OneOf.OneOf<TaggedItem, ErrorResponse>.FromT0());
+        _mediator
+            .When(mediator => mediator.Send(Arg.Any<Backend.Commands.TagItemRequest>()))
+            .Do(_ => existingItem.Tags.Add(_tagMapper.MapFromDto(Any.Pack(_textTag))));
+        _mediator.Send(Arg.Any<Backend.Commands.TagItemRequest>()).Returns(_ => existingItem);
 
         // Act
-        var response = await _sut.TagItem(tagItemRequest, testServerCallContext);
+        var response = await _sut.TagItem(tagItemRequest, _testServerCallContext);
 
         // Assert
+        await _mediator.Received(1).Send(Arg.Any<Backend.Commands.TagItemRequest>());
+        response.Item.Should().NotBeNull();
+        response.Item.TaggableItem.Should().Be(taggableItemDto);
+        response.Item.Tags.Should().Contain(Any.Pack(_textTag));
+        response.ErrorMessage.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task TagItem_ValidRequest_TaggedUnsuccessfully_ReturnsError()
+    {
+        // Arrange
+        var taggableItemDto = new TaggableItemDto { File = _fileDto };
+        var errorResponse = new ErrorResponse(ArbitraryErrorMessage);
+
+        var tagItemRequest = new TagItemRequest { Item = taggableItemDto, Tag = Any.Pack(_textTag) };
+
+        _mediator.Send(Arg.Any<Backend.Commands.TagItemRequest>()).Returns(_ => errorResponse);
+
+        // Act
+        var response = await _sut.TagItem(tagItemRequest, _testServerCallContext);
+
+        // Assert
+        await _mediator.Received(1).Send(Arg.Any<Backend.Commands.TagItemRequest>());
+        response.ErrorMessage.Should().Be(ArbitraryErrorMessage);
+        response.Item.Should().BeNull();
     }
 }
