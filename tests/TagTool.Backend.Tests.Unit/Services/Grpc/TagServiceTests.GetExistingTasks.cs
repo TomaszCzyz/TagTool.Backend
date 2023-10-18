19ea11cc26d0e28a4fa5ea65c20a4780a8e568d9
tczyz <tomczyz97@gmail.com>
2023-10-18 12:18:16 +0200

    Refactor and cleanup

--- tests/TagTool.Backend.Tests.Unit/Services/Grpc/TagServiceTests.GetExistingTasks.cs
+++ tests/TagTool.Backend.Tests.Unit/Services/Grpc/TagServiceTests.GetExistingTasks.cs
@@ -15,24 +15,26 @@ public partial class TagServiceTests
     public async Task GetExistingTasks_ValidRequest_ReturnsAllTasks()
     {
         // Arrange
+        var taskId1 = "TaskId1";
+        var actionId1 = "ActionId1";
         var cronTaskWithId1 = new CronTask
         {
-            TaskId = "TaskId1",
-            ActionId = "ActionId1",
+            TaskId = taskId1,
+            ActionId = actionId1,
             TagQuery = new TagQuery { QuerySegments = new[] { new TagQuerySegment { Tag = new TextTag { Text = "TestText" } } } },
             Cron = "* * * * *"
         };
         var eventTaskWithId1 = new EventTask
         {
-            TaskId = "TaskId1",
-            ActionId = "ActionId1",
-            Events = new[] { "TestEventName" },
+            TaskId = taskId1,
+            ActionId = actionId1,
+            Events = new[] { "TestEventName" }
         };
         var eventTaskWithId2 = new EventTask
         {
             TaskId = "TaskId2",
             ActionId = "ActionId2",
-            Events = new[] { "TestEventName" },
+            Events = new[] { "TestEventName" }
         };
 
         var mediatorResponses = new List<IJustTask>
@@ -56,8 +58,8 @@ public partial class TagServiceTests
         var reply1 = await responseStream.ReadNextAsync();
         reply1.Should().NotBeNull();
         Debug.Assert(reply1 != null, nameof(reply1) + " != null");
-        reply1.TaskId.Should().Be("TaskId1");
-        reply1.ActionId.Should().Be("ActionId1");
+        reply1.TaskId.Should().Be(taskId1);
+        reply1.ActionId.Should().Be(actionId1);
         reply1.Triggers.Should().HaveCount(2);
 
         var reply2 = await responseStream.ReadNextAsync();
