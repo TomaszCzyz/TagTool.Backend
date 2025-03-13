90e2e486a588f1f30ab4115101159ea5dfdbe646
tczyz <tomczyz97@gmail.com>
2025-03-13 23:02:16 +0100

    Add migrations

---
+++ src/TagTool.BackendNew/Migrations/20250313215645_Initial6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TagTool.BackendNew.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventTriggeredInvocableInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    InvocableType = table.Column<string>(type: "TEXT", nullable: false),
+                    InvocablePayloadType = table.Column<string>(type: "TEXT", nullable: false),
+                    Payload = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventTriggeredInvocableInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventTriggeredInvocableInfos");
+        }
+    }
+}
